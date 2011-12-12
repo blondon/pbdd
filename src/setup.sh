3dbe6a8f919673c39a86279b11a19553182841b9
thodrek <thodrek@cs.umd.edu>
2011-12-12 12:20:54 -0500

    Removed setup file

--- src/setup.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-GPP47=/fs/buzz/cs714-fa11-022/localInstalls/bin/g++
-
-$GPP47 -c -fPIC pbddop.cpp -o pbddop.o
-$GPP47 -c -fPIC pbddcache.cpp -o pbddcache.o
-$GPP47 -shared -Wl,-soname,libpbdd.so -o libpbdd.so.1  pbddcache.o pbddop.o
-
-cp libpbdd.so.1 ~/localInstalls/lib/
-cp pbdd.h ~/localInstalls/include/
